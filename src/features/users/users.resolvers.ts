119a537d926543bde2ca3b57df0000e5b2ad96cd
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-04-25 15:59:18 -0500

    fixed graphql

--- src/features/users/users.resolvers.ts
+++ src/features/users/users.resolvers.ts
@@ -1,37 +1,37 @@
 import { ContextValue } from "../..";
 
 export const usersQueryResolvers = {
-    user: async (_, {id}, { dataSources }: ContextValue) => {
+    user: async (_, { id }, { dataSources }: ContextValue) => {
         const res = await dataSources.usersApi.getUser(id);
         console.log(res);
         return res;
     },
 
-    users:async (_, __, { dataSources }: ContextValue) => {
+    users: async (_, __, { dataSources }: ContextValue) => {
         const res = await dataSources.usersApi.getUsers();
         console.log(res);
         return res;
     },
 
-    city: async (_, {id}, { dataSources }: ContextValue) => {
+    city: async (_, { id }, { dataSources }: ContextValue) => {
         const res = await dataSources.usersApi.getCity(id);
         console.log(res);
         return res;
     },
 
-    cities:async (_, __, { dataSources }: ContextValue) => {
+    cities: async (_, __, { dataSources }: ContextValue) => {
         const res = await dataSources.usersApi.getCities();
         console.log(res);
         return res;
     },
 
-    country: async (_, {id}, { dataSources }: ContextValue) => {
+    country: async (_, { id }, { dataSources }: ContextValue) => {
         const res = await dataSources.usersApi.getCountry(id);
         console.log(res);
         return res;
     },
 
-    countries:async (_, __, { dataSources }: ContextValue) => {
+    countries: async (_, __, { dataSources }: ContextValue) => {
         const res = await dataSources.usersApi.getCountries();
         console.log(res);
         return res;
@@ -39,31 +39,31 @@ export const usersQueryResolvers = {
 }
 
 export const usersMutationResolvers = {
-    addUser: async (_, {username,birthday,email, password,country_id,city_id,latitude,longitude,is_active}, { dataSources }: ContextValue) => {
-        return await dataSources.usersApi.addUser(username,birthday,email, password,country_id,city_id,latitude,longitude,is_active);
+    addUser: async (_, { username, birthday, email, password, country_id, city_id, latitude, longitude, is_active }, { dataSources }: ContextValue) => {
+        return await dataSources.usersApi.addUser(username, birthday, email, password, country_id, city_id, latitude, longitude, is_active);
     },
-    updateUser: async (_, {id,username,birthday,email, password,country_id,city_id,latitude,longitude,is_active}, { dataSources }: ContextValue) => {
-        return await dataSources.usersApi.updateUser(id,username,birthday,email, password,country_id,city_id,latitude,longitude,is_active);
+    updateUser: async (_, { id, username, birthday, email, password, country_id, city_id, latitude, longitude, is_active }, { dataSources }: ContextValue) => {
+        return await dataSources.usersApi.updateUser(id, username, birthday, email, password, country_id, city_id, latitude, longitude, is_active);
     },
     deleteUser: async (_, { id }, { dataSources }: ContextValue) => {
         return dataSources.usersApi.deleteUser(id);
     },
 
-    addCity: async (_, {name}, { dataSources }: ContextValue) => {
+    addCity: async (_, { name }, { dataSources }: ContextValue) => {
         return await dataSources.usersApi.addCity(name);
     },
-    updateCity: async (_, {id,name}, { dataSources }: ContextValue) => {
-        return await dataSources.usersApi.updateCity(id,name);
+    updateCity: async (_, { id, name }, { dataSources }: ContextValue) => {
+        return await dataSources.usersApi.updateCity(id, name);
     },
     deleteCity: async (_, { id }, { dataSources }: ContextValue) => {
         return dataSources.usersApi.deleteCity(id);
     },
-    
-    addCountry: async (_, {name}, { dataSources }: ContextValue) => {
+
+    addCountry: async (_, { name }, { dataSources }: ContextValue) => {
         return await dataSources.usersApi.addCountry(name);
     },
-    updateCountry: async (_, {id,name}, { dataSources }: ContextValue) => {
-        return await dataSources.usersApi.updateCountry(id,name);
+    updateCountry: async (_, { id, name }, { dataSources }: ContextValue) => {
+        return await dataSources.usersApi.updateCountry(id, name);
     },
     deleteCountry: async (_, { id }, { dataSources }: ContextValue) => {
         return dataSources.usersApi.deleteCountry(id);
--- src/features/users/users.type-defs.ts
+++ src/features/users/users.type-defs.ts
@@ -1,24 +1,24 @@
 export const userTypesDef = `#graphql
     type User{
-        id : Number!
-        username : String!
-        birthday : Date!
+        id: Int!
+        username: String!
+        birthday: String!
         email: String!
-        password : String!
-        country_id : Number!
-        city_id : Number!
-        latitude : Number!
-        longitude : Number!
-        is_active : Boolean!
+        password: String!
+        country_id: Int!
+        city_id: Int!
+        latitude: Float!
+        longitude: Float!
+        is_active: Boolean!
     }
 
     type City{
-        id: Number!
+        id: Int!
         name: String!
     }
 
     type Country{
-        id: Number!
+        id: Int!
         name: String!
     }
 `;
@@ -26,24 +26,24 @@ export const userTypesDef = `#graphql
 
 
 export const usersQuerysDef = `#graphql
-    user(id: Number!): User
+    user(id: Int!): User
     users: [User] 
-    city(id: Number!): City
+    city(id: Int!): City
     cities: [City]
-    country(id: Number!): Country
+    country(id: Int!): Country
     countries: [Country]
 `;
 
 export const usersMutationsDef = `#graphql
-    addUser(username : String!,birthday : Date!,email: String!,password : String!,country_id : Number!,city_id : Number!,latitude : Number!,longitude : Number!,is_active : Boolean!): RequestResponse
-    updateUser(id: Number!,username : String!,birthday : Date!,email: String!,password : String!,country_id : Number!,city_id : Number!,latitude : Number!,longitude : Number!,is_active : Boolean!): RequestResponse
-    deleteUser(id: Number!): RequestResponse
+    addUser(username: String!,birthday: String!,email: String!,password: String!,country_id: Int!,city_id: Int!,latitude: Float!,longitude: Float!,is_active: Boolean!): RequestResponse
+    updateUser(id: Int!,username: String!,birthday: String!,email: String!,password: String!,country_id: Int!,city_id: Int!,latitude: Float!,longitude: Float!,is_active: Boolean!): RequestResponse
+    deleteUser(id: Int!): RequestResponse
 
-    addCity(name : String!): RequestResponse
-    updateCity(id: Number!,name : String!): RequestResponse
-    deleteCity(id: Number!): RequestResponse
+    addCity(name: String!): RequestResponse
+    updateCity(id: Int!,name: String!): RequestResponse
+    deleteCity(id: Int!): RequestResponse
 
-    addCountry(name : String!): RequestResponse
-    updateCountry(id: Number!,name : String!): RequestResponse
-    deleteCountry(id: Number!): RequestResponse
+    addCountry(name: String!): RequestResponse
+    updateCountry(id: Int!,name: String!): RequestResponse
+    deleteCountry(id: Int!): RequestResponse
 `;
