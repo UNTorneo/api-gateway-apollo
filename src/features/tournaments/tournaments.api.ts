9f292fbaa2b46bdca44226639d65c4e8667e4d23
Daniel <dgalindoca@unal.edu.co>
2023-04-29 09:27:42 -0500

    added api features

--- src/features/tournaments/tournaments.api.ts
+++ src/features/tournaments/tournaments.api.ts
@@ -1,6 +1,8 @@
 import { AugmentedRequest, RESTDataSource } from "@apollo/datasource-rest";
 import { RequestResponse } from '../../core/interfaces/base-interfaces';
 import {Tournament, AddTournament, UpdateTournament} from './tournament/tournament.interfaces';
+import { Team, AddTeam, UpdateTeam } from './teams/teams.interfaces';
+import { Match, AddMatch, UpdateMatch } from './matches/matches.interfaces';
 import https from "https";
 
 const agent = new https.Agent({
@@ -53,22 +55,77 @@ export class TournamentApi extends RESTDataSource {
         return this.post<RequestResponse>('api/Tournament', {body: addTournament});
     }
 
-    async deleteTournament(id: String): Promise<RequestResponse> {
-        return this.delete<RequestResponse>(`api/Tournament/${id}`);
-    }
-
     async updateTournament(id: String, updateTournament: UpdateTournament): Promise<RequestResponse> {
         return this.put<RequestResponse>(`api/Tournament/${id}`, {body: updateTournament});
     }
 
+    async deleteTournament(id: String): Promise<RequestResponse> {
+        return this.delete<RequestResponse>(`api/Tournament/${id}`);
+    }
+
     //* Teams
     // Queries
+    async getTeams(): Promise<Team[]> {
+        const response = await this.get<Team[]>('api/Team');
+        return response;
+    }
+
+    async getTeam(id: String): Promise<Team> {
+        console.log('getTournament', id);
+        return this.get<Team>(`api/Team/${id}`);
+    }
 
     // Mutations
+    async addTeam(addTeam: AddTeam): Promise<RequestResponse> {
+        const response = await this.post<RequestResponse>('api/Team', {body: addTeam});
+        console.log('response :>> ', response);
+        return response;
+    }
+
+    async updateTeam(id: String, updateTeam: UpdateTeam): Promise<RequestResponse> {
+        return this.put<RequestResponse>(`api/Team/${id}`, {body: updateTeam});
+    }
+
+    async deleteTeam(id: String): Promise<RequestResponse> {
+        return this.delete<RequestResponse>(`api/Team/${id}`);
+    }
+
+    async registerTeam(teamId: String, tournamentId: String): Promise<RequestResponse> {
+        return this.patch<RequestResponse>(`api/Team/tournament-register/${teamId}/${tournamentId}`);
+    }
 
     //* Matches
     // Queries
+    async getMatches(): Promise<Match[]> {
+        const response = await this.get<Match[]>('api/Match');
+        return response;
+    }
+
+    async getMatch(id: String): Promise<Match> {
+        const response = await this.get<Match>(`api/Match/${id}`);
+        return response;
+    }
+    
+    async getTournamentMatches(tournamentId: String): Promise<Match[]> {
+        const response = await this.get<Match[]>(`api/Match/tournament-matches/${tournamentId}`);
+        return response;
+    }
+
+    async getTeamMatches(teamId: String): Promise<Match[]> {
+        const response = await this.get<Match[]>(`api/Match/team-matches/${teamId}`);
+        return response;
+    }
 
     // Mutations
+    async addMatch(addMatch: AddMatch): Promise<RequestResponse> {
+        return this.post<RequestResponse>('api/Match', {body: addMatch});
+    }
 
+    async updateMatch(id: String, updateMatch: UpdateMatch): Promise<RequestResponse> {
+        return this.put<RequestResponse>(`api/Match/${id}`, {body: updateMatch});
+    }
+
+    async deleteMatch(id: String): Promise<RequestResponse> {
+        return this.delete<RequestResponse>(`api/Match/${id}`);
+    }
 }
